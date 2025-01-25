695825cea74906c745091e9c0bfd5ed50dd009de
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2025-01-26 02:51:04 +0300

    Task4

--- Tyuiu.KushnirS.Sprint2.Task4.V12/Program.cs
+++ Tyuiu.KushnirS.Sprint2.Task4.V12/Program.cs
@@ -1,11 +1,10 @@
-﻿using Tyuiu.KushnirS.Sprint2.Task4.V12.Lib;
-namespace Tyuiu.KushnirS.Sprint2.Task4.V12
+﻿namespace Tyuiu.KushnirS.Sprint2.Task4.V12
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Word!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
