0cb67641d324e66538bb15500d1e6bde676afef7
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2025-01-26 02:39:06 +0300

    Task3

---
+++ Tyuiu.KushnirS.Sprint2.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.KushnirS.Sprint2.Task3.V1.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint2.Task3.V1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.KushnirS.Sprint2.Task3.V1.Lib;
+namespace Tyuiu.KushnirS.Sprint2.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(x);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
